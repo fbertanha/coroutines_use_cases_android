13c856a65bb393dc41a04a2c4784e24a9d404a18
Felipe Bertanha <bertanhahp@hotmail.com>
2022-01-15 11:22:02 -0300

    multithreaded coroutines

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/8_coroutines_in_different_threads.kt
@@ -0,0 +1,22 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground
+
+import kotlinx.coroutines.*
+
+/**
+ * Created by felipebertanha on 05/January/2022
+ */
+fun main() = runBlocking {
+    println("main starts")
+    joinAll(
+        async { threadSwitchingCoroutine(1, 500) },
+        async { threadSwitchingCoroutine(2, 300) }
+    )
+}
+
+suspend fun threadSwitchingCoroutine(number: Int, delay: Long) {
+    println("Coroutine $number starts work on ${Thread.currentThread().name}")
+    delay(delay)
+    withContext(Dispatchers.Default) {
+        println("Coroutine $number has finished on ${Thread.currentThread().name}")
+    }
+}
