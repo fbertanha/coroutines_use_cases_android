531079e78d759ec2a261e9a835ebebe27ebbb80d
Felipe Bertanha <bertanhahp@hotmail.com>
2022-01-17 12:54:35 -0300

    basic error handling with try-catch

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -11,18 +11,6 @@ class Perform2SequentialNetworkRequestsViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        uiState.value = UiState.Loading
 
-        try {
-            viewModelScope.launch {
-                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
-                val mostRecent = recentAndroidVersions.last()
-                val featuresOnMostRecentVersion = mockApi.getAndroidVersionFeatures(mostRecent.apiLevel)
-
-                uiState.value = UiState.Success(featuresOnMostRecentVersion)
-            }
-        } catch (e: Exception) {
-            uiState.value = UiState.Error("Network request failed!")
-        }
     }
 }
