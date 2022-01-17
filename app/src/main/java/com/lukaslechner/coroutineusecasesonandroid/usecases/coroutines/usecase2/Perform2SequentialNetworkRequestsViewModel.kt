e620d09ac5b28910379c040c6b278c55d7b7cf9d
Felipe Bertanha <bertanhahp@hotmail.com>
2022-01-17 15:00:55 -0300

    perform 2 sequential network requests

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,16 +1,32 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+
+                val mostRecentAndroidVersion = recentAndroidVersions.last()
+
+                val featuresOfMostRecentVersion =
+                    mockApi.getAndroidVersionFeatures(mostRecentAndroidVersion.apiLevel)
+
+                uiState.value = UiState.Success(featuresOfMostRecentVersion)
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Unexpected server error!")
+            }
+        }
     }
 }
