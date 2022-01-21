f28fc0b809598b3872edcaff9767fcf5ea2f2ac4
Felipe Bertanha <bertanhahp@hotmail.com>
2022-01-21 13:07:34 -0300

    use case 4, fetch android versions and all features the features of each one version. Using launch and async coroutines builders

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,52 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+                val androidVersions = recentAndroidVersions.map { androidVersion ->
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }
+                uiState.value = UiState.Success(androidVersions)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+
+                val versionsFeatures: List<Deferred<VersionFeatures>> =
+                    recentVersions.map { androidVersion ->
+                        async {
+                            mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                        }
+                    } // or .awaitAll()
+
+                uiState.value = UiState.Success(versionsFeatures.awaitAll())
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
     }
 }
