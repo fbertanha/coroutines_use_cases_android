a4ada3296b19938c5b41047f757ddc0b24d26968
Felipe Bertanha <bertanhahp@hotmail.com>
2022-01-25 10:51:04 -0300

    added exponential backoff to the retry function

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -3,8 +3,10 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import kotlin.math.max
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
@@ -27,13 +29,22 @@ class RetryNetworkRequestViewModel(
         }
     }
 
-    private suspend fun <T> retry(numberOfRetries: Int, block: suspend () -> T) : T {
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        initialDelayInMillis: Long = 100,
+        maxDelayInMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ): T {
+        var currentDelay = initialDelayInMillis
         repeat(numberOfRetries) {
             try {
                 return block.invoke()
             } catch (e: Exception) {
                 Timber.e(e)
             }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayInMillis)
         }
         return block.invoke()
     }
